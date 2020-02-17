8c5d16fc7443dc44c41679721f70f322b40cba0f
Beltro39 <beltran58d@gmail.com>
2020-02-17 10:33:30 -0500

    Primer avance Poo 17/02

--- Trabajo Poo/src/trabajo/poo/TrabajoPoo.java
+++ Trabajo Poo/src/trabajo/poo/TrabajoPoo.java
@@ -1,21 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package trabajo.poo;
-
-/**
- *
- * @author Sebas
- */
 public class TrabajoPoo {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+  public static void main(String[] args) {
+        MenuDeConsola menuInicioSesion= new MenuDeConsola("**********************\n   Inicio de sesion\n**********************");
+        MenuDeConsola menuCrearUsuario= new MenuDeConsola("**********************\n   Crear Usuario\n**********************");
+        SiguienteMenu crearUsuario= new SiguienteMenu(menuCrearUsuario, "crearUsuario");
+        menuInicioSesion.añadirOpcion(crearUsuario);
+        menuInicioSesion.lanzarMenu();
+        //MenuInicioSesion.añadirOpcion();
+        
+        //MenuInicioSesion.añadirOpcion();
+        
     }
     
 }
